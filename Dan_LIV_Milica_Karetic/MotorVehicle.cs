49f6d6fdc64cf52de6b260d40beb5039786b4c43
MilicaKaretic <milicakaretic1@gmail.com>
2020-08-20 12:02:19 +0200

    model based on class diagram added

---
+++ Dan_LIV_Milica_Karetic/MotorVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan_LIV_Milica_Karetic
+{
+    abstract class MotorVehicle
+    {
+        double MotorVolume { get; set; }
+        public int Weight { get; set; }
+        public string Category { get; set; }
+        public string MotorType { get; set; }
+        public string Color { get; set; }
+        public int MotorNumber { get; set; }
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Turn on motor and start going.");
+        }
+        public virtual void Stop()
+        {
+            Console.WriteLine("Turn off motor.");
+        }
+    }
+}
---
+++ Dan_LIV_Milica_Karetic/Tractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan_LIV_Milica_Karetic
+{
+    class Tractor : MotorVehicle
+    {
+        public double TireSize { get; set; }
+        public string Drive { get; set; }
+        public int WheelBase { get; set; }
+        
+    }
+}
